7e97f3e51caecc8111a78408c85840fadb164d39
CuteITGuy <grisetroseciel@gmail.com>
2016-03-26 16:36:06 +0700

    Create UI of TaskWindowClient

--- TaskViewModels/TaskWindowClientViewModel.cs
+++ TaskViewModels/TaskWindowClientViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using CB.Model.Common;
 using TaskDataAccess;
 using TaskModels;
@@ -10,12 +11,24 @@ namespace TaskViewModels
     public class TaskWindowClientViewModel: ViewModelBase
     {
         #region Fields
+        private bool _canLoad;
+
+        private ICommand _loadAsyncCommand;
         private readonly ITaskDataAccess _taskDataAccess = new TaskEntityDataAccess();
         private IEnumerable<TaskInfo> _tasks;
         #endregion
 
 
         #region  Properties & Indexers
+        public bool CanLoad
+        {
+            get { return _canLoad; }
+            private set { SetProperty(ref _canLoad, value); }
+        }
+
+        public ICommand LoadAsyncCommand
+            => GetCommand(ref _loadAsyncCommand, async _ => await LoadAsync(), _ => CanLoad);
+
         public IEnumerable<TaskInfo> Tasks
         {
             get { return _tasks; }
@@ -27,7 +40,9 @@ namespace TaskViewModels
         #region Methods
         public async Task LoadAsync()
         {
+            CanLoad = false;
             Tasks = await _taskDataAccess.GetAllTasksAsync();
+            CanLoad = true;
         }
         #endregion
     }
