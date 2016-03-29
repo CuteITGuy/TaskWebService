3428583644de4dc3ab6fc8a09f4d331b8df5196b
Connor <grisetroseciel@gmail.com>
2016-03-29 18:04:14 +0700

    Add test InsertFileInfo

--- DataQueryWindowClient/MainWindow.xaml.cs
+++ DataQueryWindowClient/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
 using CB.Database.SqlServer;
 using CB.Web.WebServices;
 
@@ -11,6 +13,7 @@ namespace DataQueryWindowClient
         private const string DATAQUERY_URL = "http://localhost:4975/api/dataquery";
         private const string INTEGER_URL = "http://localhost:4975/api/integer";
         private readonly DataQueryService _queryService = new DataQueryService(DATAQUERY_URL);
+        private const string INSTALLATION_DB = "Installation";
         #endregion
 
 
@@ -41,5 +44,39 @@ namespace DataQueryWindowClient
             datMain.DataContext = ints;*/
         }
         #endregion
+
+
+        private async void MainWindow_OnDrop(object sender, DragEventArgs e)
+        {
+            var files = e.Data.GetData(DataFormats.FileDrop, true) as string[];
+            if (files == null) return;
+
+            var file = files[0];
+            var args = new
+            {
+                ApplicationId = int.Parse(txtId.Text),
+                Description = txtDescription.Text,
+                Directory = Path.GetDirectoryName(file),
+                Extension = Path.GetExtension(file),
+                IsInit = false,
+                Name = Path.GetFileName(file),
+                MajorVersion = 2,
+                MinorVersion = 3,
+                BuildVersion = 3,
+                RevisionVersion = 2016
+            };
+            var query = DataRequestCollection.Create(INSTALLATION_DB, "InsertFileInfo", args);
+
+            var result = await _queryService.SendRequestsAsync(query);
+            if (_queryService.HasError)
+            {
+                MessageBox.Show(_queryService.Error);
+            }
+            else
+            {
+                var fileInfoId = Convert.ToInt32(result.GetFirstValue());
+                txtFileInfoId.Text = fileInfoId.ToString();
+            }
+        }
     }
 }
